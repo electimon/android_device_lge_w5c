6fc49532858cbcf07fcad5500b6347606376984c
Paras Nagda <pnagda@codeaurora.org>
2016-10-07 19:00:39 +0530

    w5c: mediacodec: Allow select and eventfd syscalls for 8226
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    
    Change-Id: Iff6e2f7fc0562a520f56cbf4bbce72f1af545f9c

--- device.mk
+++ device.mk
@@ -244,6 +244,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Utilities
 PRODUCT_PACKAGES += \
     charge_only_mode \
