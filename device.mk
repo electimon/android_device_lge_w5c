6a8713edb412844d8e5ba0a1b546bf3fb59a57b9
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-05 13:05:19 +0300

    w5c: Build AOSP gatekeeper and keymaster HAL
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I176106952ce57f42c8cda71740b1ab754f469641

--- device.mk
+++ device.mk
@@ -202,6 +202,16 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     librs_jni
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     p2p_supplicant_overlay.conf \
