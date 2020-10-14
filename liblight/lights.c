cd97e3a12519475ef522ce44ff2348091ed51c68
electimon <electimon@gmail.com>
2020-10-14 14:38:57 +0000

    w5c: Add LP5521 Nodes to lights
    Signed-off-by: electimon <electimon@gmail.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -41,28 +41,22 @@ static struct light_state_t g_battery;
 static int g_attention = 0;
 
 char const*const RED_LED_FILE
-        = "/sys/class/leds/red/brightness";
+        = "/sys/class/leds/R/brightness";
 
 char const*const GREEN_LED_FILE
-        = "/sys/class/leds/green/brightness";
+        = "/sys/class/leds/G/brightness";
 
 char const*const BLUE_LED_FILE
-        = "/sys/class/leds/blue/brightness";
+        = "/sys/class/leds/B/brightness";
 
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
 
-char const*const BUTTON_FILE
-        = "/sys/class/leds/button-backlight/brightness";
+char const*const LED_PATTERN_FILE
+        = "/sys/devices/i2c.68/i2c-0/0-0032/led_pattern";
 
-char const*const RED_BLINK_FILE
-        = "/sys/class/leds/red/blink";
-
-char const*const GREEN_BLINK_FILE
-        = "/sys/class/leds/green/blink";
-
-char const*const BLUE_BLINK_FILE
-        = "/sys/class/leds/blue/blink";
+char const*const LED_STRENGTH_FILE
+        = "/sys/devices/i2c.68/i2c-0/0-0032/led_current_index";
 
 /**
  * device methods
