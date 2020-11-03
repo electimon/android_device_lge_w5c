c384b42bf52ad31c76da08855d0781a9ec775ed4
electimon <electimon@gmail.com>
2020-11-03 14:21:41 +0000

    w5c: setup-makefiles: We have a vendor partition
    * thanks stricted

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -41,7 +41,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 
 # We are done!
 write_footers
