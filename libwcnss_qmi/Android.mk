676452023ce7511d9cd813cb72c3eb11f6960b48
electimon <electimon@gmail.com>
2021-01-14 00:16:59 +0100

    w5c: Use the right patch
    Change-Id: I479450d3f3b6a5dd3c10b127e1f06bd7a446f2ba

--- libwcnss_qmi/Android.mk
+++ libwcnss_qmi/Android.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := wcnss_qmi_client.c
+LOCAL_SRC_FILES := libwcnss_qmi.c
 
-LOCAL_C_INCLUDES += hardware/qcom/wlan/wcnss_service
+LOCAL_C_INCLUDES += hardware/qcom/wlan-caf/wcnss-service
 LOCAL_CFLAGS += -Wall
 
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
