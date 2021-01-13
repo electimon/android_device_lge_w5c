f42aeb21ccb2377490445bb746c8bc8c43110a3e
electimon <electimon@gmail.com>
2021-01-14 00:16:16 +0100

    w5c: ATTEMPT TO FIX CAMERA
    Change-Id: Ifcde0e41d21a9de247dd457a04c0a23c8bbbb540

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,10 +130,13 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
 BOARD_USES_LEGACY_MMAP := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
+    /system/bin/mediaserver=21 \
     /system/vendor/bin/mm-qcamera-daemon=21
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 # Number of supplementary service groups allowed by init
 TARGET_NR_SVC_SUPP_GIDS := 28
